ec52e5059c208f1f24f58ce6a493e0389dc14be2
Khalid Fouad <kfouad@c2r2p6.1337.ma>
2023-05-30 20:48:43 +0100

    push one

--- print_error.c
+++ print_error.c
@@ -1,32 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   print_error.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kfouad <kfouad@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/27 20:21:32 by kfouad            #+#    #+#             */
-/*   Updated: 2023/05/14 19:38:16 by kfouad           ###   ########.fr       */
+/*   Created: 2023/05/14 18:23:13 by kfouad            #+#    #+#             */
+/*   Updated: 2023/05/27 18:07:23 by kfouad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-
-// # include <fcntl.h>
-# include <unistd.h>
-//# include <stdio.h>
-# include <stdlib.h>
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 100
-# endif
-
-char	*get_next_line(int fd);
-char	*ft_strchr(char	*save, int c);
-int	ft_strlen(char *s);
-char	*ft_strjoin(char *save, char *buf);
-char	*newline(char *save);
-char	*cutline(char *save);
-
-#endif
+void print_error(int n)
+{
+    ft_printf("Eroor\n")
+    if (n == 1)
+        ft_printf("invalid syntax of name map file")
+    else if (n == 2)
+        ft_printf("invalid name file")
+    
+    exit();
+}
--- so_long.c
+++ so_long.c
@@ -6,51 +6,228 @@
 /*   By: kfouad <kfouad@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 00:04:41 by kfouad            #+#    #+#             */
-/*   Updated: 2023/03/01 00:36:42 by kfouad           ###   ########.fr       */
+/*   Updated: 2023/05/27 16:57:41 by kfouad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stdio.h>
 # include <fcntl.h>
 #include "get_next_line/get_next_line.h"
-int main()
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+	char	*c;
+
+	c = (char *)s;
+	i = 0;
+	if (!c)
+		return ;
+	while (i < n)
+	{
+		c[i] = 0;
+		i++;
+	}
+}
+
+void	*ft_calloc(int count, int size)
+{
+	char	*p;
+	int	r;
+
+	r = count * size;
+	p = malloc(r);
+	if (p == 0)
+		return (0);
+	else
+		ft_bzero(p, r);
+	return (p);
+}
+
+char	*ft_substr(char  *s, unsigned int start, int len)
+{
+	char	*p;
+	char	*s1;
+	int	i;
+	int	n;
+
+	s1 = (char *)s;
+	i = 0;
+	n = len;
+	if (start > ft_strlen(s1) || !s1)
+		return (ft_calloc(1, 1));
+	else if (len > ft_strlen(s1) - start)
+		n = ft_strlen(s1) - start;
+	p = malloc(n + 1);
+	while (s1[i] && i < n)
+	{
+		p[i] = s1[i + start];
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
+
+int	chhal_mn_klma(char *s, char c)
+{
+	int	i;
+	int	j;
+
+	j = 0;
+	i = 0;
+	while (s[i] == c)
+		i++;
+	while (s[i])
+	{
+		if (s[i] == c && s[i + 1] != c && s[i + 1])
+			j++;
+		i++;
+	}
+	return (j + 1);
+}
+
+char	**ft_split(char  *s, char c)
+{
+	char	**result;
+	int		i;
+	int		j;
+	int		k;
+
+	i = 0;
+	k = 0;
+	result = malloc((chhal_mn_klma((char *)s, c) + 1) * sizeof(char *));
+	while (s[i])
+	{
+		while (s[i] == c && s[i])
+			i++;
+		j = i;
+		while (s[j] != c && s[j])
+			j++;
+		if (j != i)
+			result[k++] = ft_substr(s, i, j - i);
+		i = j;
+	}
+	result[k] = (char *) '\0';
+	return (result);
+}
+
+
+char	*ft_strdup(const char *s1)
+{
+	char	*s;
+	char	*p;
+	int		i;
+
+	s = (char *)s1;
+	p = (char *)malloc ((ft_strlen(s) + 1) * sizeof(char));
+	i = 0;
+	if (!s[i])
+		return (NULL);
+	while (s[i])
+	{
+		p[i] = s[i];
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
+
+int len_map(int fd)
+{
+    int len;
+
+    len = 0;
+    while(get_next_line(fd))   
+        len++;
+    close(fd);
+    return (len);
+}
+
+int main(int ac, char **av)
 {
     int fd;
     int i;
-    char **ptr;
-    char *pt;
-    i = 0;
+    int len;
+    char *raw_map;
+    char    **map;
+    char *line;
+    
     fd = open("map.ber", O_RDWR);
-    while (get_next_line(fd))
-        i++;
-    close(fd);
+    i = 0;
+    raw_map = ft_calloc(1, 1);
+    len = len_map(fd);
     fd = open("map.ber", O_RDWR);
-    ptr = malloc((i + 1) * sizeof (char *));
-    i = 0;
-    pt = get_next_line(fd);
-    while (pt)
+    while (i < len)
     {
-        ptr[i++] = pt;
-        pt = get_next_line(fd);
-    }
-    ptr[i] = 0;
-    i = 0;
-    int j = 0;
-    while (ptr[i])
-    {
-        j = 0;
-        while(ptr[i][j])
-        {
-            if (ptr[i][j] == '\n')
-                ptr[i][j] = '\0';
-            j++;
-        }
+        line = get_next_line(fd);
+        raw_map = ft_strjoin(raw_map, line);
+        free(line);
         i++;
     }
-    i =0;
-    while(ptr[i])
+    i = 0;
+    map = ft_split(raw_map, '\n');
+    free(raw_map);
+    while (map[i] != 0)
     {
-        printf("%s\n", ptr[i]);
-       i++;
+        printf("%s\n", map[i++]);
     }
+    printf("%d\n", check_size_map(map));
+    printf("%d\n", check_wall_map(map));
+    // {
+    //     printf("%s\n", ptr[i]);
+    //    i++;
+    // }
 }
+
+int check_size_map(char **ptr)
+{
+    int size;
+    int i;
+
+    size = ft_strlen(ptr[0]);
+    i = 0;
+    while (ptr[++i])
+    {
+        if (size != ft_strlen(ptr[i]))
+            return (0);
+    }
+    return (1);
+}
+
+int check_wall_map(char **ptr)
+{
+    int hight;
+    int j;
+    int i;
+    int size;
+
+    j = -1;
+    hight = 0;
+    size = ft_strlen(ptr[0]) - 1;
+    
+    while(ptr[hight])
+        hight++;
+    while (ptr[++j])
+    {
+        if (ptr[j][0] != '1' || ptr[j][size] != '1')
+            return (0);
+        i = -1;
+        while ((j == 0 || j == hight - 1) && ptr[j][++i])
+        {
+            if (ptr[j][i] != '1')
+                return (0);
+        }
+    }
+    return (1);
+}
+
+// void ft_check_name(char *str)
+// {
+//     int i;
+    
+//     i = 0;
+//     while (str[i] != '.' && str[i] != '\0')
+//         i++;
+//     if (ft_strncmp(&str[i], ".ber", 4) == 1)
+//         print_error(2);
+// }
